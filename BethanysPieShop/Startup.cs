07ce8ae0df38e5b0fdf5dff49d22c0fcf1751968
apisceva <aljona.gromova@gmail.com>
2022-03-28 07:55:04 -0400

    new changes with shopping Cart

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -29,6 +29,9 @@ namespace BethanysPieShop
             
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
 
             services.AddControllersWithViews(x => x.SuppressAsyncSuffixInActionNames = false)
@@ -44,6 +47,7 @@ namespace BethanysPieShop
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
